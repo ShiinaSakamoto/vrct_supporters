047fc766fecf9d37a36dac00af572b52c22b2e92
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2025-01-17 10:22:50 +0900

    [TEST]

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-
     build: {
       outDir: path.resolve(__dirname, "docs"),
       rollupOptions: {
