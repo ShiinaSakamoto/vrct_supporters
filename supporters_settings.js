31102ee794d0c5e0c168a02178ce53b9e815ee48
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2026-04-30 09:05:56 +0900

    [Update] 2026-04-30

--- supporters_settings.js
+++ supporters_settings.js
@@ -78,14 +78,14 @@ const generateMonthRange = (range_mode, date_str1, date_str2) => {
 
 // 0埋め 最終更新（とする）日時
 const year = "2026";
-const month = "03";
-const day = "27";
+const month = "04";
+const day = "30";
 
-const hour = "17";
+const hour = "08";
 const min = "59";
 const sec = "59";
 
-const calc_latest_month = "2026-03";
+const calc_latest_month = "2026-04";
 
 export const supporters_settings = {
     last_updated_utc_date: new Date(`${year}-${month}-${day}T${hour}:${min}:${sec}+09:00`),
