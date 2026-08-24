69389e01c38ffdc7a7c6bee1343fbedf987fbb3e
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2026-08-24 16:04:53 +0900

    [Update] 2026-08-24

--- supporters_settings.js
+++ supporters_settings.js
@@ -79,9 +79,9 @@ const generateMonthRange = (range_mode, date_str1, date_str2) => {
 // 0埋め 最終更新（とする）日時
 const year = "2026";
 const month = "08";
-const day = "14";
+const day = "24";
 
-const hour = "13";
+const hour = "15";
 const min = "59";
 const sec = "59";
 
